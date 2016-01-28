a95b22afd086d8957652b00723dca3e7ff8098fb
4rob2 <reasoner55@hotmail.com>
2016-01-28 15:54:30 -0800

    add specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,22 @@
+
 $(document).ready(function() {
   $("form#paddle").submit(function(event) {
-  var inputA = parseInt($("input#ending").val());
+  event.preventDefault();
+  var count = $("#ending").val()
 
-  for(var i=1;inputA >= i; i++) {
-    if (i % 15 === 0)
-     document.write("pingpong" + "<br/>");
-
-   else if
-     (i % 5 === 0)
-      document.write("pong" + "<br/>");
-
-   else if (i % 3 === 0)
-     document.write("ping" + "<br/>");
-     else
-       document.write((i) + "<br/>");
-
-    event.preventDefault();
-   }
-   var ask = prompt("Want to play again?");
-      if (ask = "true"){
-        return
-
-    {
-
+    for (var i = 1; i <= count; i++) {
+      if (i % 15 === 0) {
+        $("#results").append("<li>"+"Ping-Pong"+"</li>");
       }
-    }
-
-  })
+      else if (i % 3 === 0) {
+        $("#results").append("<li>"+"Ping"+"</li>");
+      }
+      else if (i % 5 === 0) {
+        $("#results").append("<li>"+"Pong"+"</li>");
+      }
+      else{
+        $("#results").append("<li>"+i+"</li>");
+      }
+    };
+  });
 });
